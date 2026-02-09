1260d0172535ec8d398fdf1f4043ddc7b8b6a7a0
zarusirig <enepsters101@gmail.com>
2026-02-09 15:10:13 +0545

    Add AdSense verification script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,15 +113,13 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="//www.google-analytics.com" />
         <link rel="dns-prefetch" href="//www.googletagmanager.com" />
 
-        {/* Google AdSense (conditional) */}
-        {process.env.NEXT_PUBLIC_ADS_ENABLED === 'true' && process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID && (
-          <Script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
-            crossOrigin="anonymous"
-            strategy="afterInteractive"
-          />
-        )}
+        {/* Google AdSense verification */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6191764023643150"
+          crossOrigin="anonymous"
+          strategy="beforeInteractive"
+        />
 
         {/* Google tag (gtag.js) */}
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X90GQYNEHW" />
